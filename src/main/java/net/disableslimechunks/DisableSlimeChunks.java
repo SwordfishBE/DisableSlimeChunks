b7c56d7d78fddebd86418ee103f798363b2aec18
Jimmy A. <6824251+SwordfishBE@users.noreply.github.com>
2026-08-26 15:33:21 +0200

    Update DisableSlimeChunks.java

--- src/main/java/net/disableslimechunks/DisableSlimeChunks.java
+++ src/main/java/net/disableslimechunks/DisableSlimeChunks.java
@@ -1,15 +1,24 @@
 package net.disableslimechunks;
 
 import net.fabricmc.api.ModInitializer;
+import net.fabricmc.loader.api.FabricLoader;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 public final class DisableSlimeChunks implements ModInitializer {
 	public static final String MOD_ID = "disable_slime_chunks";
-	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
+	private static final String LOG_PREFIX = "[DisableSlimeChunks]";
+	private static final Logger LOGGER = LoggerFactory.getLogger("DisableSlimeChunks");
 
 	@Override
 	public void onInitialize() {
-		LOGGER.info("Disable Slime Chunks initialized");
+		String version = FabricLoader.getInstance()
+				.getModContainer(MOD_ID)
+				.orElseThrow()
+				.getMetadata()
+				.getVersion()
+				.getFriendlyString();
+
+		LOGGER.info("{} Mod initialized. Version: {}", LOG_PREFIX, version);
 	}
 }
